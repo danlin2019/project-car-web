8964e0e97d5d151a4d7dda61148f045302eec384
danlin <asmob2011@gmail.com>
2025-01-14 23:14:39 +0800

    no message

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // 開發中 還是產品路徑
+  base: process.env.NODE_ENV === 'project-car-web/',
   plugins: [react(),svgr()],
   server:{
     port:3000,
